c4f9a282b5cfa2be75abd4b9edbc86b486fce412
TimBolhoeve <tim.bolhoeve@deboer-group.com>
2022-08-16 16:00:51 +0100

    changed static file location

--- project/settings.py
+++ project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "./page/static"), 
+        os.path.join(BASE_DIR, "./static"), 
     ]
 
 
