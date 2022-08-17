332c7a6dcd5773dad46757b379962b758e28cede
TimBolhoeve <tim.bolhoeve@deboer-group.com>
2022-08-17 10:28:43 +0100

    added dropdown in additem-type

--- page/forms.py
+++ page/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
+# First param is the value (displayed on the home page in light grey), second param is what the End-User sees
+TYPE_CHOICES= [
+    ('Electronics', 'Electronics'),
+    ('Logos', 'Logos'),
+    ]
+
 class ItemForm(forms.Form):
     name = forms.CharField()
     description = forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
     price = forms.CharField(max_length=100)
-    type = forms.CharField()
+    type = forms.CharField(label='Type of product', widget=forms.Select(choices=TYPE_CHOICES))
     stock = forms.CharField()
     image = forms.ImageField()
